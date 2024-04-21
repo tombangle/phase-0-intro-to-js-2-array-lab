f6eb95993b7b0d9db4da704bcfcccfd806cefd70
Tom Bangle <tombangle@gmail.com>
2024-04-20 18:38:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = [`Milo`, `Otis`, `Garfield`];
+
+function destructivelyAppendCat(Twinkles) {
+    cats.push(Twinkles);
+}
+
+function destructivelyPrependCat(Twizzler) {
+    cats.unshift(Twizzler);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Zoey) {
+    return [...cats, Zoey];
+}
+
+function prependCat(Bella) {
+    return [Bella, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
